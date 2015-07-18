936831f01d1ec2e69ec0b7c8e924b9547de2a87c
n38803 <shaunthompson@MacBook-Pro.home>
2015-07-17 21:47:32 -0400

    Delete function working now.

--- CPM1507W2/ParseStarterProject/ParseStarterProject/UserViewController.h
+++ CPM1507W2/ParseStarterProject/ParseStarterProject/UserViewController.h
@@ -9,21 +9,33 @@
 #import <UIKit/UIKit.h>
 #import "Parse/Parse.h"
 
-@interface UserViewController : UIViewController
+@interface UserViewController : UIViewController<UITableViewDataSource, UITableViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *nameInput;
 @property (weak, nonatomic) IBOutlet UITextField *numberInput;
+
+
+
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
+
+
 @property (weak, nonatomic) IBOutlet UIButton *addButton;
+
+
+@property (weak, nonatomic) IBOutlet UISwitch *editToggle;
 
 
 @property (weak, nonatomic) IBOutlet UILabel *loginInfo;
 @property (weak, nonatomic) IBOutlet UIButton *logOutButton;
 
 
+
+
 @property NSString *username;
-@property NSArray *contactArray;
+@property NSMutableArray *contactArray;
+@property NSString *contactname;
+@property NSString *contactnumber;
 
 
 //@property (weak, nonatomic) IBOutlet UITableView *contactTable;
--- CPM1507W2/ParseStarterProject/ParseStarterProject/UserViewController.m
+++ CPM1507W2/ParseStarterProject/ParseStarterProject/UserViewController.m
@@ -23,10 +23,10 @@
     // determine user & parse info
     [self detectUser];
     
-    // determine size of object array
-    
     // query parse data
     [self performSelector:@selector(queryParse)];
+    
+    
     
     // convert object to string & populate string array
     
@@ -40,18 +40,33 @@
 - (void) queryParse {
     
     PFQuery *query = [PFQuery queryWithClassName:self.username];
-    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error)
+    {
         
         if (!error){
-            self.contactArray = [[NSArray alloc] initWithArray:objects];
+            self.contactArray = [[NSMutableArray alloc] initWithArray:objects];
         }
         
-        NSLog(@"%@", self.contactArray);
+        // refresh table to populate parse data
         [self.tableView reloadData];
-        
+        // NSLog(@"%@", self.contactArray);
+ 
     }];
 }
 
+
+
+
+- (IBAction)editContact:(id)sender {
+    
+    UISwitch *editSwitch = (UISwitch *)sender;
+    
+    if ([editSwitch isOn]) {
+        [_tableView setEditing:YES animated:YES];
+    } else {
+        [_tableView setEditing:NO animated:YES];
+    }
+}
 
 
 - (void) detectUser {
@@ -77,21 +92,21 @@
 
 - (IBAction)addContact:(id)sender {
     
-    NSString *name = self.nameInput.text;
-    NSString *number = self.numberInput.text;
+    _contactname = self.nameInput.text;
+    _contactnumber = self.numberInput.text;
     NSNumberFormatter *format = [[NSNumberFormatter alloc] init];
     format.numberStyle = NSNumberFormatterDecimalStyle;
-    NSNumber *convert = [format numberFromString:number];
+    NSNumber *convert = [format numberFromString:_contactnumber];
     
     
     PFObject *contact = [PFObject objectWithClassName:self.username];
-    contact[@"name"] = name;
+    contact[@"name"] = _contactname;
     contact[@"number"] = convert;
     
     [contact saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
         if (succeeded) {
             // The object has been saved.
-            NSLog(@"%@ has been added", name);
+            NSLog(@"%@ has been added", _contactname);
         } else {
             // There was a problem, check error.description
         }
@@ -100,7 +115,7 @@
     self.nameInput.text = @"";
     self.numberInput.text = @"";
     
-    // TODO ------------------------ REFRESH OR UPDATE TABLE VIEW
+    [_tableView reloadData];
     
 }
 
@@ -157,6 +172,56 @@
     return cell;
     
     
+}
+
+
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return UITableViewCellEditingStyleDelete;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete)
+    {
+        
+         // Save text of the selected cell:
+         UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+         NSString *selectedContact = cell.textLabel.text;
+        
+        
+        // adjust array
+        [_contactArray removeObjectAtIndex:indexPath.row];
+        
+        
+        
+        // adjust parse
+        PFQuery *query = [PFQuery queryWithClassName:_username];
+        [query whereKey:@"name" equalTo:selectedContact];
+        [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+            if (!error) {
+                // The find succeeded.
+                NSLog(@"Successfully retrieved %@", selectedContact);
+                // Do something with the found objects
+                for (PFObject *object in objects) {
+                    NSLog(@"%@", object.objectId);
+                    
+                    [object deleteInBackground];
+
+                }
+
+            } else {
+                // Log details of the failure
+                NSLog(@"Error: %@ %@", error, [error userInfo]);
+            }
+        }];
+        
+        // refresh parse
+        [self queryParse];
+        
+        // delete row from tableview
+        [_tableView deleteRowsAtIndexPaths:[NSMutableArray arrayWithObjects:indexPath, nil] withRowAnimation:UITableViewRowAnimationFade];
+    }
 }
 
 // -----------------------------------------------
